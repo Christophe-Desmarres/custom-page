0ad727d31f0b6723e8c84d36a28c35f276a902ef
chrisdmar <chrisdmar12@gmail.com>
2023-04-05 18:46:56 +0200

    install vue.js

---
+++ custom-page/src/components/templates/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="footer">
+        <h1>This is a footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+
+}
+</script>
+
+<style>
+
+</style>
